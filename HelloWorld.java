7aaaf7ecf7cd78ff1ef8caba33264a9d164ccf0f
Daniel Nascimento <danielhn@gmail.com>
2020-12-07 17:57:14 -0500

    First Java code and first compiler

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,13 @@
-public class HelloWorld {
+// public class HelloWorld {
 	
+// 	public static void main(String[] args) {
+// 		System.out.println("Hello World! It's me Kathryn!");
+// 	}
+// }
+
+public class HelloWorld {
+
 	public static void main(String[] args) {
-		System.out.println("Hello World! It's me Kathryn!");
+		System.out.println("Hello Worlds! It's me Daniel and I love something!");
 	}
 }

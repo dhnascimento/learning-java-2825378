5421af5ebce9ae7b9f53f265853a1d44b93d37da
Daniel Nascimento <danielhn@gmail.com>
2020-12-08 18:01:02 -0500

    Coding along the video instructions from chapter 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+package src;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        String studentFirstName = "Daniel";
+        String studentLastName = "Nascimento";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
+        boolean hasPerfectAttendance = true;
+
+        // System.out.println(studentFirstName);
+        // System.out.println(studentLastName);
+        // System.out.println(studentFirstInitial);
+        // System.out.println(studentLastInitial);
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPE of " + studentGPA);
+        System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + studentGPA);
+
     }
 }
